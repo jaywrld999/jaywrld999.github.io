31603715f13aa8c249251d66a9e2651b0f4e811c
jaywrld999 <jaylon.webb.student@madisonpsb.org>
2022-02-11 19:14:29 +0000

    added platforms and collectables to the game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -38,7 +38,7 @@
 
         
         createPlatform(400, 460);
-        createPlatform(300, 200, 0.3)
+        createPlatform(400, 460,)
         createPlatform(300, 200, 0.3)
         createPlatform(300, 200, 0.3)
         createPlatform(500, 500, 0.3, 10)
